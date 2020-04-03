7981c927a611fa4c1c0cf8f3f1962b5783e072f6
Randee Orion <randeegood@gmail.com>
2020-04-02 21:35:04 -0700

    append works insert before needs love

--- data-structures/linkedList/linked-list.js
+++ data-structures/linkedList/linked-list.js
@@ -47,18 +47,47 @@ class LinkedList {
     console.log(str)
     return str;
   }
+
+  append(val) {
+    let current = this.head;
+    if(!current){
+      myList.insert('J');
+    }
+    while(current.next != null) {
+        current = current.next;
+      }
+    let newNode = new Node(val);
+    current.next = newNode;
+  }
+
+  insertBefore(val, newVal){
+    let current = this.head;
+    console.log(current);
+    console.log(current.next.val, val)
+    while(current.next.val !== val){
+      console.log('inside while loop');
+      current = current.next;
+    }
+    let newNode = new Node(newVal);
+    current.prev = newNode;
+  }
+
+  
 }
 
 let myList = new LinkedList();
 
 myList.insert('A');
-// myList.insert('B');
-// myList.insert('C');
-// myList.insert('D');
+myList.insert('B');
+myList.insert('C');
+myList.insert('D');
 // myList.insert('E');
 
-// myList.includes('B');
-// myList.includes('F');
+myList.includes('B');
+myList.includes('F');
+
+myList.append('Q'),
+myList.insertBefore('B', 'E')
 
 myList.toString();
 
