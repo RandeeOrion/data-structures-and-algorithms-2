3669d86e42b8e6114bbc95ff99d089ba9721e3f4
Randee Orion <randeegood@gmail.com>
2020-04-28 21:42:16 -0700

    good code.

--- code-challenges/multiBracketValidation/multi_bracket_validation.js
+++ code-challenges/multiBracketValidation/multi_bracket_validation.js
@@ -1,44 +1,43 @@
-'use strict '
+'use strict';
+
+const Stack = require('../stacksAndQueues/stack.js');
 
 function multiBracketValidation(str){
   if(typeof str !== 'string'){
     return false;
   }
   let charArr = str.split('');
-  if(charArr.length <= 1){
-    return false;
-  }
-  let openBracket = /[{([]/gm;
-  let openCounter = 0;
-  let closeBracket = /[})]]/gm;
-  let closeCounter = 0;
+  let stackOBrackets = new Stack();
+  let openBracket = /[{[(]/m;
   for (let i = 0; i < charArr.length; i++ ){
+    console.log('charArr at i', charArr[i]);
+    // console.log('openBrack test', openBracket.test(charArr[i]));
     if (openBracket.test(charArr[i]) ){
-      openCounter ++;
-      console.log('open counter', openCounter);
-    }
-    if (closeBracket.test(charArr[i]) ) {
-      console.log('inside second if');
-      closeCounter ++;
-      console.log('close counter', closeCounter)
+      stackOBrackets.push(charArr[i]);
+      console.log('pushed to stackOBrackets', charArr[i]);
+    } else if (charArr[i] === '}' || charArr[i] === ']' || charArr[i] === ')') {
+      let poppedVal = stackOBrackets.pop();
+      // console.log(poppedVal.val, charArr[i]);
+      if (charArr[i] === "}" && poppedVal.val !== '{') return false;
+      if (charArr[i] === "]" && poppedVal.val !== '[') return false;
+      if (charArr[i] === ")" && poppedVal.val !== '(') return false;
     }
   }
-  if (openCounter === closeBracket){
-    return true;
-  }
-  else return false;
+  if (stackOBrackets.top) return false;
+  return true;
 }
 
 
-const good = '(){}[]';
-const bad = '({[}}]';
-const ugly = '{{{sd)))'
+const good = '({[]})';
+// const bad = '({[}}]';
+// const pretendGood = '([])'
 console.log('---------------- GOOD ---------------');
 console.log(multiBracketValidation(good));
-console.log('---------------- BAD ---------------');
-console.log(multiBracketValidation(bad));
-console.log('---------------- UGLY ---------------');
-console.log(multiBracketValidation(ugly));
+// console.log('---------------- BAD ---------------');
+// console.log(multiBracketValidation(bad));
+// console.log('---------------- PRETENDGOOD ---------------');
+// console.log(multiBracketValidation(pretendGood));
+
 
 
 module.exports = multiBracketValidation;
