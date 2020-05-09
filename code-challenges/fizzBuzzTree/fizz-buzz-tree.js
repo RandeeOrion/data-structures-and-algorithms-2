3d611822e08ec24d40ba31ed4095496a1eed2140
Randee Orion <randeegood@gmail.com>
2020-05-09 16:47:11 -0700

    tests are now passing

--- code-challenges/fizzBuzzTree/fizz-buzz-tree.js
+++ code-challenges/fizzBuzzTree/fizz-buzz-tree.js
@@ -1,7 +1,5 @@
 'use strict';
 
-const BT = require('../tree/binarySearchTree.js').BinaryTree;
-
 class Node {
   constructor (val) {
     this.val = val;
@@ -21,14 +19,14 @@ treeRoot.left = new Node(9);
 treeRoot.left.left = new Node(3);
 treeRoot.left.right = new Node(2);
 
-const fizBuzz = (node) => {
+const fizzBuzz = (node) => {
   //Variables to make this easier, a new tree and a string to concat whatever is needed for the value of the new nodes in the new tree
   let str = '';
 
   //checking the value of the current node and reassigning value where needed
-  if(node.val % 2 === 0) str += 'Fizz';
-  if(node.val % 3 === 0) str += 'Buzz';
-  if(node.val % 3 !== 0 && node.val % 2 !== 0) str = `${node.val}`;
+  if(node.val % 3 === 0) str += 'Fizz';
+  if(node.val % 5 === 0) str += 'Buzz';
+  if(node.val % 3 !== 0 && node.val % 5 !== 0) str = `${node.val}`;
 
   // creating the new node for the new tree
   let newTreeRoot = new Node(str);
@@ -44,3 +42,6 @@ const fizBuzz = (node) => {
 
   return newTreeRoot;
 }
+
+module.exports = {Node, fizzBuzz};
+
