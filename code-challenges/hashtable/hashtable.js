f4293196fbebf1839f28e567631226ef579f0ee3
Randee Orion <randeegood@gmail.com>
2020-06-23 18:21:13 -0700

    add and get functions added

--- code-challenges/hashtable/hashtable.js
+++ code-challenges/hashtable/hashtable.js
@@ -13,5 +13,27 @@ class HashTable {
     for (var i = 0; i< string.length; i++) {
       total += H * total + string.charCodeAt(i);
     }
+
+    total %= this.table.length;
+    if (total <1) {
+      this.table.length -1
+    }
+    return parseInt(total);
+  }
+
+  add(key, value) {
+    const pos = this.hash(key);
+    this.table[pos] = {key: value};
+  }
+
+  get(key) {
+    const pos = this.hash(key);
+    let index = 0;
+    while (this.table[pos][index] !=  key){
+      if(this.table[pos][index] !== undefined){
+        return this.table[pos][index]
+      } else return undefined; 
+    }
+    index ++
   }
 }
